8774048030dca17a56621a0040f4dbf62cff5ff4
Philip Tettey <82468726+iAmQuamz@users.noreply.github.com>
2023-03-23 11:53:06 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_sign(int n);
 
 int _abs(int);
 
-int print_last_digit(int); I
+int print_last_digit(int);
 
 void jack_bauer(void);
 
@@ -21,5 +21,3 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
-
-void print_times_table(int n);
