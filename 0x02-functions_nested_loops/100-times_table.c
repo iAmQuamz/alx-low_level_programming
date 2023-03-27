9edf7812694e9fbdbd6b1bfe16d88637e1fe5155
Philip Tettey <82468726+iAmQuamz@users.noreply.github.com>
2023-03-27 08:34:56 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,48 @@
-#include “main.h”
+#include "main.h"
+
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
- */
+ * print_times_table -prints the n times table, starting with 0
+ * @n: number to be passed with the function
+*/
+
 void print_times_table(int n)
 {
-	int i, j, res;
+	int i, j, k;
 
-	if (!(n > 15 || n < 0))
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			k = j * i;
+			if (j == 0)
+				_putchar('0' + k);
+			else
 			{
-				res = (i * j);
-				if (j != 0)
+				_putchar(',');
+				_putchar(' ');
+				if (k < 10)
 				{
-					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				}
-				if (res < 10 && j != 0)
+				else if (k < 100)
 				{
 					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 				else
-					_putchar((res % 10) + '0');
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
